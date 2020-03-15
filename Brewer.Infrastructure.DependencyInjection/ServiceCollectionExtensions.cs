4672ed6eae287e753f3adf37bda79aeb8d36c8c2
Ismael Carlos Velten <ismaelcarlosvelten@gmail.com>
2020-03-15 14:48:46 -0300

    Removing EntityFramework
    Co-authored-by: André Brandemburg dos Anjos <aanjos.brandemburg@gmail.com>

--- Brewer.Infrastructure.DependencyInjection/ServiceCollectionExtensions.cs
+++ Brewer.Infrastructure.DependencyInjection/ServiceCollectionExtensions.cs
@@ -1,18 +1,10 @@
 ﻿using System;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
-using Brewer.Infrastructure.Data;
-using Microsoft.EntityFrameworkCore;
 
 namespace Brewer.Infrastructure.DependencyInjection
 {
     public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection AddBrewerContext(this IServiceCollection services, IConfiguration configuration)
-        {
-            var connectionString = configuration.GetConnectionString("BrewerConnection");
-
-            return services.AddDbContext<BrewerContext>(options => options.UseSqlServer(connectionString));
-        }
     }
 }
